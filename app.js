4a838ed66c4f8f646a2f6102ee55fb4687c434b6
Vytautas <vytautasjaz@gamil.com>
2020-09-26 12:29:29 +0300

    first commit

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ const table = document.getElementById('table');
 let fingerRotation = 0;
 let fingerX = 0;
 let fingerY = 0;
+
+// when movement reaches 10 steps, finger stops moving
 let movementX = 0;
 let movementY = 0;
 
-if (fingerRotation == 0) {
+if (fingerRotation === 0) {
   btnForward.addEventListener('click', () => {
-    //   let fingerPosition = finger.getBoundingClientRect().x;
+    console.log(fingerRotation);
     fingerX += 32.5;
     movementX += 1;
     finger.style.left = `${fingerX}px`;
   });
-} else if (fingerRotation == 90) {
+} else if (fingerRotation === 90) {
   fingerY += 32.5;
   movementY += 1;
   finger.style.top = `${fingerY}px`;
